2f97930f1361ba38d79b67b85e5b584803267aee
Mike Webster <mwebster@wyzant.com>
2019-04-16 23:40:30 -0500

    fix the get and post. update router setup.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"time"
 
+	"github.com/jmoiron/sqlx"
+	"github.com/mike-webster/anon-solicitor/data"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/mike-webster/anon-solicitor/controllers"
 )
@@ -12,18 +15,28 @@ import (
 func main() {
 	ctx := context.Background()
 	log.Print("Sleeping to allow db setup...")
-	time.Sleep(10 * time.Second)
+	time.Sleep(3 * time.Second)
 
-	controllers.StartServer(ctx)
+	err := data.CreateTables(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := sqlx.Open("mysql", "root@tcp(db:3306)/anon_solicitor?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	es := data.EventService{DB: db}
+
+	controllers.StartServer(ctx, es)
 
 	// wrap := main.DBWrapper{}
 	// wrap.Get()
 
 	// ctx = context.WithValue(ctx, "db", wrap)
-	// err := data.CreateTables(ctx)
-	// if err != nil {
-	// 	panic(err)
-	// }
+
 }
 
 // func mwAttachDB(c *gin.Context) {
--- controllers/controllers-v1.go
+++ controllers/controllers-v1.go
@@ -2,19 +2,32 @@ package controllers
 
 import (
 	"context"
+	"log"
 	"net/http"
 
 	gin "github.com/gin-gonic/gin"
+	anon "github.com/mike-webster/anon-solicitor"
 )
 
-func StartServer(ctx context.Context) {
-	r := setupRouter(ctx)
+type ContextKey string
+
+func (c ContextKey) String() string {
+	return string(c)
+}
+
+var eventServiceKey ContextKey = "EventService"
+var testKey ContextKey = "test"
+
+func StartServer(ctx context.Context, es anon.EventService) {
+	r := setupRouter(ctx, es)
 	r.Run("0.0.0.0:3001")
 }
 
-func setupRouter(ctx context.Context) *gin.Engine {
+func setupRouter(ctx context.Context, es anon.EventService) *gin.Engine {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
+
+	r.Use(setDependencies(es))
 
 	r.GET("/", getHomeV1)
 	r.GET("/events", getEventsV1)
@@ -32,17 +45,25 @@ func setupRouter(ctx context.Context) *gin.Engine {
 	return r
 }
 
+func setDependencies(es anon.EventService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set(eventServiceKey.String(), es)
+		c.Next()
+	}
+}
+
 func getHomeV1(c *gin.Context) {
 	c.HTML(http.StatusOK, "master.html", gin.H{"title": "Anon Solicitor"})
 }
 
 func getEventsV1(c *gin.Context) {
-	contextDB := c.Value("db")
-	if contextDB == nil {
+	es := c.Value(eventServiceKey.String())
+	if es == nil {
 		c.HTML(500, "error.html", gin.H{"msg": "missing db in context"})
 		return
 	}
-
+	c.HTML(200, "error.html", gin.H{"msg": "found service"})
+	return
 	// db, ok := contextDB.(DBWrapper)
 	// if !ok {
 	// 	c.HTML(500, "error.html", gin.H{"msg": "db conversion error"})
@@ -61,36 +82,56 @@ func getEventsV1(c *gin.Context) {
 }
 
 func postEventsV1(c *gin.Context) {
-	// contextDB := c.Value("db")
-	// if contextDB == nil {
-	// 	c.HTML(500, "error.html", gin.H{"msg": "missing db in context"})
-	// 	return
-	// }
+	untypedES := c.Value(eventServiceKey.String())
+	if untypedES == nil {
+		c.HTML(500, "error.html", gin.H{"msg": "missing db in context"})
+		return
+	}
 
-	// db, ok := contextDB.(DBWrapper)
-	// if !ok {
-	// 	c.HTML(500, "error.html", gin.H{"msg": "db conversion error"})
-	// 	return
-	// }
+	es, ok := untypedES.(anon.EventService)
+	if !ok {
+		c.HTML(http.StatusInternalServerError, "error.html", gin.H{"msg": "couldnt cast db"})
 
-	// newEvent := Event{}
-	// err := c.Bind(&newEvent)
-	// if err != nil {
-	// 	log.Printf("Error binding object: %v", err)
-	// 	c.HTML(http.StatusBadRequest, "error.html", gin.H{"msg": err})
+		return
+	}
 
-	// 	return
-	// }
+	postEvent := anon.EventPostParams{}
+	err := c.Bind(&postEvent)
+	if err != nil {
+		log.Printf("Error binding object: %v", err)
+		c.HTML(http.StatusBadRequest, "error.html", gin.H{"msg": err})
 
-	// createdAt := time.Now().UTC()
-	// res, err := db.Get().Exec("INSERT INTO events (id, title, description, time, created_at, modified_at) VALUES (?,?,?,?,?,?)",
-	// 	newEvent.ID,
-	// 	newEvent.Title,
-	// 	newEvent.Description,
-	// 	newEvent.Time,
-	// 	createdAt,
-	// 	createdAt,
-	// )
+		return
+	}
+
+	log.Printf("posted event: %v", postEvent)
+
+	newEvent := anon.Event{
+		Title:       postEvent.Title,
+		Description: postEvent.Description,
+		Time:        postEvent.Time,
+		UserID:      1,
+	}
+
+	log.Printf("saving event: %v", newEvent)
+
+	err = es.CreateEvent(&newEvent)
+	if err != nil {
+		log.Printf("error creating event: %v", err)
+		c.HTML(http.StatusInternalServerError, "error.html", gin.H{"msg": err})
+
+		return
+	}
+
+	posted := es.GetEvent(newEvent.ID)
+	if posted == nil {
+		log.Printf("error getting created event: %v", err)
+		c.HTML(http.StatusInternalServerError, "error.html", gin.H{"msg": "couldnt retrieve saved event", "id": newEvent.ID})
+
+		return
+	}
+
+	c.HTML(http.StatusOK, "event.html", gin.H{"title": "Anon Solicitor | Event", "event": posted})
 
 	// if err != nil {
 	// 	c.HTML(http.StatusBadRequest, "error.html", gin.H{"msg": err})
@@ -100,7 +141,7 @@ func postEventsV1(c *gin.Context) {
 	// newEvent.ID = id
 
 	//c.HTML(http.StatusOK, "event.html", gin.H{"title": "Anon Solicitor | Event", "event": newEvent})
-	c.HTML(http.StatusNotImplemented, "error.html", gin.H{"msg": "...coming soon..."})
+	//c.HTML(http.StatusNotImplemented, "error.html", gin.H{"msg": "...coming soon..."})
 }
 
 func putEventsV1(c *gin.Context) {
