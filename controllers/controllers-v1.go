8f056f8a50b8331b819d63871f8e8a46c7bb602f
Mike Webster <mwebster@wyzant.com>
2019-04-17 00:38:29 -0500

    wont need this

--- controllers/controllers-v1.go
+++ controllers/controllers-v1.go
@@ -32,7 +32,6 @@ func setupRouter(ctx context.Context, es anon.EventService) *gin.Engine {
 	r.GET("/", getHomeV1)
 	r.GET("/events", getEventsV1)
 	r.POST("/events", postEventsV1)
-	r.PUT("/events/:id", putEventsV1)
 	r.POST("/events/:id/feedback", postFeedbackV1)
 	r.GET("/config", getConfigV1)
 	r.PUT("/config", putConfigV1)
@@ -141,76 +140,6 @@ func postEventsV1(c *gin.Context) {
 
 	//c.HTML(http.StatusOK, "event.html", gin.H{"title": "Anon Solicitor | Event", "event": newEvent})
 	//c.HTML(http.StatusNotImplemented, "error.html", gin.H{"msg": "...coming soon..."})
-}
-
-func putEventsV1(c *gin.Context) {
-	c.HTML(http.StatusNotImplemented, "error.html", gin.H{"msg": "...coming soon..."})
-	//contextDB := c.Value("db")
-	// if contextDB == nil {
-	// 	c.HTML(500, "error.html", gin.H{"msg": "missing db in context"})
-	// 	return
-	// }
-
-	// db, ok := contextDB.(DBWrapper)
-	// if !ok {
-	// 	c.HTML(500, "error.html", gin.H{"msg": "db conversion error"})
-	// 	return
-	// }
-
-	// eventUpdates := EventPutParams{}
-	// err := c.Bind(&eventUpdates)
-	// if err != nil {
-	// 	log.Printf("Error binding object: %v", err)
-	// 	c.HTML(http.StatusBadRequest, "error.html", gin.H{"msg": err})
-
-	// 	return
-	// }
-
-	// existing := Event{}
-	// db.Get().Where("id = ?", eventUpdates.ID).Find(&existing)
-
-	// // TODO: Verify that the event selected from the database
-	// //       belongs to the requesting user.  Maybe (after auth)
-	// //       pull the user id from the token and add it to the
-	// //       where clause?
-
-	// // This isn't working because I'm not creating events correctly yet
-	// // if existing.OrganizingUser.ID < 1 {
-	// // 	c.HTML(http.StatusUnauthorized, "error.html", gin.H{"msg": err, "explain": "didn't find an event"})
-
-	// // 	return
-	// // }
-
-	// if len(eventUpdates.Description) > 0 {
-	// 	existing.Description = eventUpdates.Description
-	// }
-
-	// if len(eventUpdates.Title) > 0 {
-	// 	existing.Title = eventUpdates.Title
-	// }
-
-	// if eventUpdates.OrganizerID > 0 {
-	// 	newUser := User{}
-	// 	db.Get().Where("id = ?", eventUpdates.OrganizerID).Find(&newUser)
-
-	// 	if newUser.UserID < 1 {
-	// 		c.HTML(http.StatusBadRequest, "error.html", gin.H{"msg": "new user wasn't found"})
-
-	// 		return
-	// 	}
-
-	// 	existing.User = newUser
-	// }
-
-	// min := time.Time{}
-
-	// if eventUpdates.Time != min {
-	// 	existing.Time = eventUpdates.Time
-	// }
-
-	// db.Get().Save(&existing)
-
-	// c.HTML(http.StatusOK, "event.html", gin.H{"title": "Anon Solicitor | Event", "event": existing})
 }
 
 func postFeedbackV1(c *gin.Context) {
