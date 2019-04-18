cb79b1509acbd27afb193c1c9b46c681717c5c4b
Mike Webster <mwebster@wyzant.com>
2019-04-17 21:40:31 -0500

    send the jwt

--- controllers/controllers-v1.go
+++ controllers/controllers-v1.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -288,8 +289,8 @@ func sendEmail(email string, tok string, eventName string, eventID int64) error 
 	message := gomail.NewMessage()
 	message.SetHeader("From", "anno-solicitor@wyzant.com")
 	message.SetHeader("To", email)
-
-	fbPath := fmt.Sprintf("http://%v/events/%v/feedback/%v", cfg.Host, eventID, tok)
+	jwt := tokens.GetJWT(cfg.Secret, tok)
+	fbPath := fmt.Sprintf("http://%v/events/%v/feedback/%v", cfg.Host, eventID, jwt)
 	body := fmt.Sprintf("<html><body><h3>Hey! We'd like to hear what you think!</h3><p>No worries - it's totally anonymous! Click <a href='%v'>here</a> to submit your feedback and see what everyone else thought!</p><p>Click <a href='%v'>here</a> to let us know that you didn't attend.</p><p>Thanks so much!</p></body></html>", fbPath, fbPath+"/absent")
 
 	message.SetHeader("Title", fmt.Sprintf("You've been invited to give anonymous feedback about: %v", eventName))
