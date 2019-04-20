dce30d1dad763f9402228c28e96e9495dcc2b09b
Mike Webster <mwebster@wyzant.com>
2019-04-19 21:27:50 -0500

    cleanup

--- data/data.go
+++ data/data.go
@@ -9,60 +9,10 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-var _db *sqlx.DB
-
-func DB() *sqlx.DB {
-	if _db == nil {
-		db, err := sqlx.Open("mysql", "root@tcp(db:3306)/anon_solicitor?charset=utf8&parseTime=True&loc=Local")
-		if err != nil {
-			fmt.Println("DB - db error 2: ", err)
-
-			return nil
-		}
-
-		_db = db
-	}
-
-	err := _db.Ping()
-	if err != nil {
-		fmt.Println("DB - ping err: ", err)
-
-		return nil
-	}
-
-	return _db
-}
-
-func DropTables(ctx context.Context) error {
-	log.Println("-- Deleting tables")
-	queries := []string{
-		"DROP TABLE IF EXISTS feedback;",
-		"DROP TABLE IF EXISTS events;",
-		"DROP TABLE IF EXISTS questions;",
-	}
-
-	db, err := sqlx.Open("mysql", "root@tcp(db:3306)/anon_solicitor?charset=utf8&parseTime=True&loc=Local")
-	if err != nil {
-		fmt.Println("DB - db error 2: ", err)
-
-		return nil
-	}
-	defer db.Close()
-
-	for _, q := range queries {
-		_, err = db.Exec(q)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 func CreateTables(ctx context.Context, db *sqlx.DB) error {
-	dropTables, _ := ctx.Value("DropTables").(bool)
-	if dropTables {
-		DropTables(ctx)
+	dt, _ := ctx.Value("DropTables").(bool)
+	if dt {
+		dropTables(ctx)
 	}
 	fmt.Println("-- Creating tables")
 
@@ -118,6 +68,32 @@ func createTable(ctx context.Context, db *sqlx.DB, tableName string, tableSchema
 	_, err := db.Exec(tableSchema)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func dropTables(ctx context.Context) error {
+	log.Println("-- Deleting tables")
+	queries := []string{
+		"DROP TABLE IF EXISTS feedback;",
+		"DROP TABLE IF EXISTS events;",
+		"DROP TABLE IF EXISTS questions;",
+	}
+
+	db, err := sqlx.Open("mysql", "root@tcp(db:3306)/anon_solicitor?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		fmt.Println("DB - db error 2: ", err)
+
+		return nil
+	}
+	defer db.Close()
+
+	for _, q := range queries {
+		_, err = db.Exec(q)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
