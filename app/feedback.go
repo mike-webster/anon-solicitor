e36582e7416d2f348e6b6b1c78d81025641b2122
Mike Webster <mwebster@wyzant.com>
2019-06-12 19:58:19 -0500

    add db tags

--- app/feedback.go
+++ app/feedback.go
@@ -2,9 +2,11 @@ package app
 
 // Feedback represents a user's opinion on an event.
 type Feedback struct {
-	ID      int64
-	Tok     string
-	EventID int64
+	ID      int64  `db:"id"`
+	Tok     string `db:"tok"`
+	EventID int64  `db:"event_id"`
+	Content string `db:"content"`
+	Absent  bool   `db:"absent"`
 }
 
 type FeedbackService interface {
--- app/question.go
+++ app/question.go
@@ -5,13 +5,13 @@ import "time"
 // Question fd
 type Question struct {
 	ID        int64
-	EventID   int64
-	Title     string
+	EventID   int64  `db:"event_id"`
+	Title     string `db:"title"`
 	Content   string
-	Answers   string // This is for "options" for the question, delimited by ";;"
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt time.Time
+	DBAnswers sql.NullString `db:"answers"` // This is for "options" for the question, delimited by ";;"
+	CreatedAt time.Time      `db:"created_at"`
+	UpdatedAt mysql.NullTime `db:"updated_at"`
+	DeletedAt mysql.NullTime `db:"deleted_at"`
 }
 
 type QuestionPostParams struct {
