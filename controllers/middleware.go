935e8a5603ff9e9b5d61a96dcc315160d96359fb
Mike Webster <mwebster@wyzant.com>
2019-06-13 22:52:08 -0500

    fix the terrible validation messages

--- controllers/middleware.go
+++ controllers/middleware.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 
 	"github.com/mike-webster/anon-solicitor/email"
 
@@ -14,6 +17,7 @@ import (
 	"github.com/mike-webster/anon-solicitor/data"
 	"github.com/mike-webster/anon-solicitor/env"
 	"github.com/mike-webster/anon-solicitor/tokens"
+	"gopkg.in/go-playground/validator.v8"
 )
 
 func setDependencies(ctx context.Context, db *sqlx.DB) gin.HandlerFunc {
@@ -128,14 +132,22 @@ func setStatus() gin.HandlerFunc {
 					fmt.Println(fmt.Sprint("updated status: ", respStatus))
 				}
 			} else {
-				log.Print("responseStatus not found - defaulting to 500")
+				log.Println("responseStatus not found - defaulting to 500")
 			}
 
-			ret := map[string]interface{}{}
-			ret["msg"] = "sorry - we encountered an error and we're working on it!"
-			if len(c.Errors) > 0 {
-				ret["errors"] = c.Errors
+			ret := map[string]string{}
+
+			for _, e := range c.Errors {
+				switch e.Type {
+				case gin.ErrorTypeBind:
+					helpful := e.Err.(validator.ValidationErrors)
+					for _, err := range helpful {
+						ret[err.Field] = ValidationErrorToText(err)
+					}
+				default:
+				}
 			}
+
 			c.AbortWithStatusJSON(respStatus, ret)
 
 			return
@@ -149,3 +161,134 @@ func setStatus() gin.HandlerFunc {
 		// as a 200... but I think errors would get logged?
 	}
 }
+
+// Below is borrowed from some very kind stranger.
+// sauce: https://github.com/gin-gonic/gin/issues/430#issuecomment-446113460
+var (
+	ErrorInternalError = errors.New("whoops something went wrong")
+)
+
+func UcFirst(str string) string {
+	for i, v := range str {
+		return string(unicode.ToUpper(v)) + str[i+1:]
+	}
+	return ""
+}
+
+func LcFirst(str string) string {
+	return strings.ToLower(str)
+}
+
+func Split(src string) string {
+	// don't split invalid utf8
+	if !utf8.ValidString(src) {
+		return src
+	}
+	var entries []string
+	var runes [][]rune
+	lastClass := 0
+	class := 0
+	// split into fields based on class of unicode character
+	for _, r := range src {
+		switch true {
+		case unicode.IsLower(r):
+			class = 1
+		case unicode.IsUpper(r):
+			class = 2
+		case unicode.IsDigit(r):
+			class = 3
+		default:
+			class = 4
+		}
+		if class == lastClass {
+			runes[len(runes)-1] = append(runes[len(runes)-1], r)
+		} else {
+			runes = append(runes, []rune{r})
+		}
+		lastClass = class
+	}
+
+	for i := 0; i < len(runes)-1; i++ {
+		if unicode.IsUpper(runes[i][0]) && unicode.IsLower(runes[i+1][0]) {
+			runes[i+1] = append([]rune{runes[i][len(runes[i])-1]}, runes[i+1]...)
+			runes[i] = runes[i][:len(runes[i])-1]
+		}
+	}
+	// construct []string from results
+	for _, s := range runes {
+		if len(s) > 0 {
+			entries = append(entries, string(s))
+		}
+	}
+
+	for index, word := range entries {
+		if index == 0 {
+			entries[index] = UcFirst(word)
+		} else {
+			entries[index] = LcFirst(word)
+		}
+	}
+	justString := strings.Join(entries, " ")
+	return justString
+}
+
+func ValidationErrorToText(e *validator.FieldError) string {
+	word := Split(e.Field)
+
+	switch e.Tag {
+	case "required":
+		return fmt.Sprintf("%s is required", word)
+	case "max":
+		return fmt.Sprintf("%s cannot be longer than %s", word, e.Param)
+	case "min":
+		return fmt.Sprintf("%s must be longer than %s", word, e.Param)
+	case "email":
+		return fmt.Sprintf("Invalid email format")
+	case "len":
+		return fmt.Sprintf("%s must be %s characters long", word, e.Param)
+	}
+	return fmt.Sprintf("%s is not valid", word)
+}
+
+// This method collects all errors and submits them to Rollbar
+// func Errors() gin.HandlerFunc {
+
+// 	return func(c *gin.Context) {
+// 		c.Next()
+// 		// Only run if there are some errors to handle
+// 		if len(c.Errors) > 0 {
+// 			for _, e := range c.Errors {
+// 				// Find out what type of error it is
+// 				switch e.Type {
+// 				case gin.ErrorTypePublic:
+// 					// Only output public errors if nothing has been written yet
+// 					if !c.Writer.Written() {
+// 						c.JSON(c.Writer.Status(), gin.H{"Error": e.Error()})
+// 					}
+// 				case gin.ErrorTypeBind:
+// 					errs := e.Err.(validator.ValidationErrors)
+// 					list := make(map[string]string)
+// 					for _,err := range errs {
+// 						list[err.Field] = ValidationErrorToText(err)
+// 					}
+
+// 					// Make sure we maintain the preset response status
+// 					status := http.StatusBadRequest
+// 					if c.Writer.Status() != http.StatusOK {
+// 						status = c.Writer.Status()
+// 					}
+// 					c.JSON(status, gin.H{"Errors": list})
+
+// 				default:
+// 					// Log all other errors
+// 					rollbar.RequestError(rollbar.ERR, c.Request, e.Err)
+// 				}
+
+// 			}
+// 			// If there was no public or bind error, display default 500 message
+// 			if !c.Writer.Written() {
+// 				c.JSON(http.StatusInternalServerError, gin.H{"Error": ErrorInternalError.Error()})
+// 			}
+// 		}
+// 	}
+// }
